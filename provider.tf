a9dfeb8778a5d38a6cf0ecc329efd54106a068ed
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-02-25 13:26:38 +0530

    adding state file to s3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket  = "eks-tf-state-file-bucket"
+    key     = "eks/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
