1a86cf0c21f5137761b8f8e03ddd63d032d45a89
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-02-26 11:49:45 +0530

    adding auto assign ip to eks

--- main.tf
+++ main.tf
@@ -8,8 +8,9 @@ module "vpc" {
   azs            = ["${var.aws_region}a", "${var.aws_region}b"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
 }
 
 # IAM Role for EKS Nodes
