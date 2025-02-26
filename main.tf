ff75e1421a2ca6eee59b5dca8252a9a5014b8e5e
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-02-26 11:32:35 +0530

    removing s3 bucket code manually creating it

--- main.tf
+++ main.tf
@@ -1,38 +1,3 @@
-# Create an S3 bucket for Terraform state storage
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "eks-tf-state-file-bucket"
-}
-
-# Enable versioning on the S3 bucket
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Block public access to the S3 bucket
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-# Create a DynamoDB table for Terraform state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 # VPC module
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
--- outputs.tf
+++ outputs.tf
@@ -32,13 +32,3 @@ output "ecr_repository_url" {
   description = "URL of the ECR repository"
   value       = module.ecr.repository_url
 }
-
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state"
-  value       = aws_s3_bucket.terraform_state.id
-}
-
-output "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for Terraform state locking"
-  value       = aws_dynamodb_table.terraform_locks.name
-}
