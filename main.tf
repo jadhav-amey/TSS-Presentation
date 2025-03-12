9aa1b8fe54c677480ee8eca60a035e33863d6bd7
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-03-12 16:00:44 +0530

    destorying the modules

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ module "vpc" {
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   cluster_name                   = var.cluster_name
-  cluster_version                = "1.29"
   subnet_ids                     = module.vpc.public_subnets
   vpc_id                         = module.vpc.vpc_id
   enable_irsa                    = true # Enables IAM Roles for Service Accounts
