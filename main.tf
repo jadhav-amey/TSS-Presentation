2ef5667417b77fe5e7ecab5c59a0052081836ec0
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-03-17 14:13:10 +0530

    fixing errors

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# VPC module
+# VPC Module
 resource "aws_vpc" "eks_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -35,20 +35,83 @@ resource "aws_route_table_association" "public_association" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# EKS Cluster Module
+# EKS Module
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# IAM Role for EKS Worker Nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# EKS Cluster
 resource "aws_eks_cluster" "eks" {
   name     = var.eks_cluster_name
   role_arn = aws_iam_role.eks_role.arn
+
   vpc_config {
     subnet_ids = [aws_subnet.public_subnet.id]
   }
 }
 
+# EKS Node Group
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name   = aws_eks_cluster.eks.name
-  node_role_arn  = aws_iam_role.eks_role.arn
+  node_role_arn  = aws_iam_role.eks_node_role.arn
   subnet_ids     = [aws_subnet.public_subnet.id]
   instance_types = [var.instance_type]
+
   scaling_config {
     desired_size = var.desired_size
     max_size     = var.max_size
@@ -56,7 +119,7 @@ resource "aws_eks_node_group" "eks_nodes" {
   }
 }
 
-# ECR Module
+# ECR Repository
 resource "aws_ecr_repository" "app_repo" {
   name = var.ecr_name
 }
--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "eks_cluster_name" {
 output "ecr_repository_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.terraform_state.id
-}
